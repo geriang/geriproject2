517d3641670dd69869c65a15b32032a5ac25005a
gerillima@gmail.com <gerillima@gmail.com>
2023-02-14 20:31:04 +0800

    added axios.get axios.post axios.put and axios.delete codes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId
 
 const mongoUri = process.env.MONGO_URI;
 
@@ -23,18 +24,101 @@ async function main() {
     const db = await connect(mongoUri, "project")
 
 
-// FOR DEMONSTRATION WITH THE SAMPLE_AIRBNB database
-app.get('/', async function (req, res) {
-    // eqv: db.collection('listings_and_reviews').find().limit(10);
-    const listings = await db.collection("user_details").find().limit(10).toArray();
-    // if we use res.send and the first parameter is a JavaScript object or array, Express will automatically send back JSON
-    res.status(200); // indicate later when we send, we want the status code to be 200
-    res.send(listings);  // a normal res.send or res.render by default is status code 200
-})
+    // GET
+    app.get('/', async function (req, res) {
+        // eqv: db.collection('listings_and_reviews').find().limit(10);
+        const listings = await db.collection("user_details").find().limit(10).toArray();
+        // if we use res.send and the first parameter is a JavaScript object or array, Express will automatically send back JSON
+        res.status(200); // indicate later when we send, we want the status code to be 200
+        res.send(listings);  // a normal res.send or res.render by default is status code 200
+    })
+
+    // POST
+    app.post("/user_details", async function (req, res) {
+        let userId = req.body.userId;
+        let ceaNo = req.body.ceaNo;
+        let mobile = req.body.contact.mobile
+        let email = req.body.contact.email
+        let username = req.body.login.username
+        let password = req.body.login.password
+        let first = req.body.name.first
+        let last = req.body.name.last
+        let timestamp = new Date().toISOString()
+
+        try {
+            let result = await db.collection("user_details").insertOne({
+
+                'userId': userId,
+                'ceaNo': ceaNo,
+                "contact": { 'mobile': mobile, 'email': email },
+                "login": { 'username': username, 'password': password },
+                "name": { 'first': first, 'last': last },
+                'created': timestamp
+
+            });
+            res.status(200);
+            res.send(result);
+        } catch (e) {
+            res.status(500);
+            res.send({
+                error: "Internal server error. Please contact administrator"
+            });
+            console.log(e);
+        }
+    });
+
+    // PUT
+    app.put("/user_details/:id", async function (req, res) {
+
+        try {
+            let userId = req.body.userId;
+            let ceaNo = req.body.ceaNo;
+            let mobile = req.body.contact.mobile
+            let email = req.body.contact.email
+            let username = req.body.login.username
+            let password = req.body.login.password
+            let first = req.body.name.first
+            let last = req.body.name.last
+            let timestamp = new Date().toISOString()
+
+            let result = await db.collection("user_details").updateOne({
+                '_id': ObjectId(req.params.id)
+            }, {
+                '$set': {
+
+                    'userId': userId,
+                    'ceaNo': ceaNo,
+                    'contact': { 'mobile': mobile, 'email': email },
+                    'login': { 'username': username, 'password': password },
+                    'name': { 'first': first, 'last': last },
+                    'created': timestamp
+
+                }
+            });
+
+            res.status(200);
+            res.send(result);
+
+        } catch (e) {
+            res.status(500);
+            res.send(e);
+            console.log(e);
+
+        }
+    })
+    // DELETE
+    app.delete("/user_details/:id", async (req, res) => {
+         await db.collection("user_details").deleteOne({
+          "_id": ObjectId(req.params.id)
+        });
+        res.status(200);
+        res.send({
+          message: "OK"
+        });
+      });
+
 }
-// app.get('/', function(req,res){
-//     res.send("Hello World")
-// })
+
 
 main()
 
@@ -42,42 +126,3 @@ app.listen(5000, function () {
     console.log("app started")
 })
 
-app.post("/user_details", async function(req, res) {
-    let userId = req.body.description;
-    let ceaNo = req.body.food;
-    let datetime = new Date(req.body.datetime) || new Date();
-
-    try{
-        let result = await db.collection("user_details").insertOne({
-
-              userId : "0000001",
-              ceaNo : "R006232H",
-              contact : {
-                mobile : "84430486",
-                email: "geriang@huttonsgroup.com"
-              },
-               login : {
-                "username": "jery",
-                "password": "password1!"
-              },
-              name : {
-                "first": "Geri",
-                "last": "Ang"
-              },
-              created: {
-                "$timestamp": {
-                  "t": 0,
-                  "i": 0
-                }
-              }
-        });
-        res.status(200);
-        res.send(result);
-    } catch (e) {
-        res.status(500);
-        res.send({
-            error: "Internal server error. Please contact administrator"
-        });
-        console.log(e);
-    }
-});
